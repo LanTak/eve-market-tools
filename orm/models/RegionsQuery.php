c69e18ad2fb212359096d703a5ddc800b7c09c82
Ty Hudson <ty@tysoft.me>
2017-11-07 18:31:44 -0500

    models updated for new data

---
+++ orm/models/RegionsQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+use Base\RegionsQuery as BaseRegionsQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'regions' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class RegionsQuery extends BaseRegionsQuery
+{
+
+}
